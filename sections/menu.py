5c39cae3ff86122a816eb002b99624269d468e70
Richard Sherriff <sherriff8@gmail.com>
2020-12-05 15:33:40 +0000

    Added animated menu!

--- sections/menu.py
+++ sections/menu.py
@@ -7,23 +7,44 @@ import tile_types
 from ui.menuUI import MenuUI
 import os.path
 
-menu_panel_xp_file = 'images/menuPanel.xp'
+menu_panel_xp_file = 'images/menuPanel'
+
 
 class Menu(Section):
     def __init__(self, engine, x,y,width, height):
         super().__init__(engine, x, y, width, height)
 
-        if os.path.isfile(menu_panel_xp_file):
-            xp_file = gzip.open(menu_panel_xp_file)
-            raw_data = xp_file.read()
-            xp_file.close()
+        self.animated_tiles =list()
+        for i in range(0,3):
+            if os.path.isfile(menu_panel_xp_file  + str(i+1) + '.xp'):
+                xp_file = gzip.open(menu_panel_xp_file  + str(i+1)+ '.xp')
+                raw_data = xp_file.read()
+                xp_file.close()
 
-            xp_data = xp_loader.load_xp_string(raw_data)
+                xp_data = xp_loader.load_xp_string(raw_data)
 
-            self.tiles = np.full((self.width, self.height), fill_value=tile_types.blank, order="F")
+                self.animated_tiles.append(np.full((self.width, self.height), fill_value=tile_types.blank, order="F"))
 
-            for h in range(0,self.height):
-                for w in range(0,self.width):
-                    self.tiles[w,h]['graphic']=  xp_data['layer_data'][0]['cells'][w][h]
+                for h in range(0,self.height):
+                    for w in range(0,self.width):
+                        self.animated_tiles[i][w,h]['graphic']=  xp_data['layer_data'][0]['cells'][w][h]
 
-            self.ui = MenuUI(self, x,y, self.tiles["graphic"])
+        self.ui = MenuUI(self, x,y, self.animated_tiles[0]["graphic"])
+        self.selected_tiles = 0
+        self.frame_length = 20
+        self.time_on_frame =0
+
+    def render(self, console):
+        if self.time_on_frame > self.frame_length:
+            self.selected_tiles += 1
+            self.selected_tiles %= 3
+            self.time_on_frame = 0
+
+        if len(self.animated_tiles[self.selected_tiles]) > 0:
+            if self.invisible == False:
+                console.tiles_rgb[self.x : self.x + self.width, self.y: self.y + self.height] = self.animated_tiles[self.selected_tiles]["graphic"]
+
+            if self.ui is not None:
+                self.ui.render(console)
+
+        self.time_on_frame += 0.16
--- ui/menuUI.py
+++ ui/menuUI.py
@@ -10,12 +10,12 @@ class MenuUI(UI):
         super().__init__(section, x, y)
         self.elements = list()
 
-        bd = [27, 19,10,3] #Button Dimensions
+        bd = [27, 15,10,3] #Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
         one_button = Button(x=bd[0], y=bd[1], width=bd[2], height=bd[3], click_action=CloseMenu(self.section.engine), tiles=button_tiles )
         self.add_element(one_button)
 
-        bd = [27, 23,10,3] #Button Dimensions
+        bd = [27, 19,10,3] #Button Dimensions
         button_tiles = tiles[bd[0]:bd[0] + bd[2], bd[1]:bd[1] + bd[3]]
         one_button = Button(x=bd[0], y=bd[1], width=bd[2], height=bd[3], click_action=EscapeAction(self.section.engine), tiles=button_tiles )
         self.add_element(one_button)
